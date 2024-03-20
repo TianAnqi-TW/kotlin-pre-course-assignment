2ae46ee04266612e314947c73d3f639ae5e260cf
anqi.tian <anqi.tian@thoughtworks.com>
2024-03-20 16:31:01 +0800

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,17 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test");
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    implementation("io.mockk:mockk:1.10.6")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.0")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 
 }
 
